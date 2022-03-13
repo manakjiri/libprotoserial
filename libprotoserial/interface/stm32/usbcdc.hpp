2855b84bd11be1469cdcd3a63fa2fad881ee23a6
Jirka Manak <jirka.manak@email.cz>
2022-03-13 15:44:38 +0100

    housekeeping

--- libprotoserial/interface/stm32/usbcdc.hpp
+++ libprotoserial/interface/stm32/usbcdc.hpp
@@ -1,3 +1,23 @@
+/*
+ * This file is a part of the libprotoserial project
+ * https://github.com/georges-circuits/libprotoserial
+ * 
+ * Copyright (C) 2022 Jiří Maňák - All Rights Reserved
+ * For contact information visit https://manakjiri.eu/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/gpl.html>
+ */
 
 #ifndef _SP_INTERFACE_USBCDC
 #define _SP_INTERFACE_USBCDC
