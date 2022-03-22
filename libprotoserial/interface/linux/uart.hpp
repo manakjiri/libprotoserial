536e6a41212fd138dc343b2f00e62126ec85c263
Jirka Manak <jirka.manak@email.cz>
2022-03-22 11:04:58 +0100

    fixed the character substitution, it was an incorrect setting in termios

--- libprotoserial/interface/linux/uart.hpp
+++ libprotoserial/interface/linux/uart.hpp
@@ -126,7 +126,10 @@ class uart_interface : public buffered_parsed_interface<Header, Footer>
             if (num_bytes > 0)
             {
                 for (int i = 0; i < num_bytes; i++)
+                {
                     this->put_single_received((byte)read_buf[i]);
+                    //std::cout << (int)read_buf[i] << std::endl;
+                }
             }
         } while (num_bytes == sizeof(read_buf));
     }
@@ -146,7 +149,7 @@ class uart_interface : public buffered_parsed_interface<Header, Footer>
         cfsetospeed (&tty, speed);
         cfsetispeed (&tty, speed);
 
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        /* tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
         // disable IGNBRK for mismatched speed tests; otherwise receive break
         // as \000 chars
         tty.c_iflag &= ~IGNBRK;         // disable break processing
@@ -163,7 +166,32 @@ class uart_interface : public buffered_parsed_interface<Header, Footer>
         tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
         tty.c_cflag |= parity;
         tty.c_cflag &= ~CSTOPB;
-        //tty.c_cflag &= ~CRTSCTS;
+        //tty.c_cflag &= ~CRTSCTS; */
+
+
+        tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+        tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+        tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+        tty.c_cflag |= CS8; // 8 bits per byte (most common)
+        tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+        tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+        tty.c_lflag &= ~ICANON;
+        tty.c_lflag &= ~ECHO; // Disable echo
+        tty.c_lflag &= ~ECHOE; // Disable erasure
+        tty.c_lflag &= ~ECHONL; // Disable new-line echo
+        tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+        tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+        tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+        tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+        // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+        // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+
+        tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+        tty.c_cc[VMIN] = 0;
+
 
         if (tcsetattr (fd, TCSANOW, &tty) != 0)
         {
--- tests/linux_uart.cpp
+++ tests/linux_uart.cpp
@@ -1,5 +1,5 @@
 
-#include "libprotoserial/fragmentation.hpp"
+/* #include "libprotoserial/fragmentation.hpp"
 #include "tests/helpers/random.hpp"
 
 using namespace sp::literals;
@@ -42,6 +42,50 @@ int main(int argc, char const *argv[])
     //cout << "Sent" << endl;
 
     return 0;
+} */
+
+#define SP_BUFFERED_DEBUG
+
+#include "libprotoserial/interface.hpp"
+#include "tests/helpers/random.hpp"
+
+using namespace sp::literals;
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    sp::uart_interface interface("/dev/ttyACM0", B115200, 0, 2, 10, 64, 256);
+    
+    int i = 0;
+    interface.receive_event.subscribe([&](sp::fragment f){
+        cout << "RX" << i << ": " << f << endl;
+    });
+
+    for (; i < 1; ++i)
+    {
+        sp::bytes data = random_bytes(5, interface.max_data_size());
+        //sp::bytes data = random_bytes(3);
+        
+        auto f = sp::fragment(1, sp::bytes(data));
+        cout << "TX" << i << ": " << f << endl;
+        interface.write(std::move(f));
+
+        
+        int loops = 10;
+        while (loops--)
+            interface.main_task();
+    }
+    
+
+    //cout << "Sent" << endl;
+
+    return 0;
 }
 
 
+
+/* 
+900510793
+900510793
+900510793
+ */
